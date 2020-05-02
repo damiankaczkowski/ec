9d8a873655b5a489afb6494ee5ac22655d6b530e
Evan Lojewski <github@meklort.com>
2020-05-02 14:41:02 -0600

    lemp9: Allow the camera default power to be configured by the user.

--- src/board/system76/common/power.c
+++ src/board/system76/common/power.c
@@ -7,7 +7,19 @@
 #include <board/power.h>
 #include <board/pmc.h>
 #include <board/pnp.h>
+#include <common/config.h>
 #include <common/debug.h>
+
+config_t power_ccd_en = {
+    .config_short = "Default Camera State",
+    .config_desc = "When set, the camera is enabled on system reset",
+    .value = {
+        .min_value = 0,
+        .max_value = 1,
+        .value = 1 /* Default to enabled */
+    },
+    .set_callback = NULL,
+};
 
 // Platform does not currently support Deep Sx
 #define DEEP_SX 0
@@ -80,6 +92,10 @@ enum PowerState calculate_power_state(void) {
 #else
     return POWER_STATE_DS5;
 #endif
+}
+
+void power_init(void) {
+    config_register(&power_ccd_en);
 }
 
 void update_power_state(void) {
@@ -258,6 +274,9 @@ static void power_cpu_reset(void) {
     pnp_enable();
     //TODO: reset KBC and touchpad states
     kbled_reset();
+
+    gpio_set(&CCD_EN, power_ccd_en.value.value);
+
 }
 
 void power_event(void) {
